84962eea4b08580d5b7fc5caced9af29b9112429
MayTheStar <maibadr200@gmail.com>
2025-03-03 14:37:53 +0300

    Initial Commit

---
+++ Purely/PurelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurelyApp.swift
+//  Purely
+//
+//  Created by May Bader Alotaibi on 03/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PurelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
