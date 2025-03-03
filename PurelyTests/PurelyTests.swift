e23a80f27ee3c16b790799eb1bf415c59f95f580
Falghannami <falghannami24@twq.idserve.net>
2025-03-03 14:37:53 +0300

    last Changes

---
+++ PurelyTests/PurelyTests.swift
@@ -0,0 +1,17 @@
+//
+//  PurelyTests.swift
+//  PurelyTests
+//
+//  Created by May Bader Alotaibi on 03/09/1446 AH.
+//
+
+import Testing
+@testable import Purely
+
+struct PurelyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
